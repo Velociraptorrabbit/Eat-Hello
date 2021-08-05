2d5a2cbbcf47c835433f8b4ac11d3c5e1d833c32
Jae Kim <jaekimr@gmail.com>
2021-08-05 16:43:44 -0400

    cookie working

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,7 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/userModel');
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 const userController = {};
-
 
 // userController.readParams = (req, res, next) => {
 //   const { username, password } = req.body;
@@ -12,57 +11,61 @@ const userController = {};
 // };
 
 userController.createUser = async (req, res, next) => {
-  try { 
+  try {
     const newUser = await User.create(req.body);
     res.locals.id = newUser._id;
     return next();
-  } catch(err) {
-    return res.send('User already exists');
+  } catch (err) {
+    return res.send("User already exists");
   }
 };
 
-userController.getUser = async (req,res,next) =>{
-  const { username , password } = req.body;
+userController.getUser = async (req, res, next) => {
+  const { username, password } = req.body;
   const result = await User.findOne({ username }, (err, user) => {
     if (!user || err) {
-      console.log('user not found');
-      return res.send('Invalid Username or Password'); 
+      console.log("user not found");
+      return res.send("Invalid Username or Password");
     }
   });
-  console.log('result', result);
+  console.log("result", result);
   res.locals.password = password;
   res.locals.dbPassword = result.password;
+  res.locals.db_id = result._id;
   return next();
 };
 
-userController.getFoodHistory = async (req,res,next) =>{
+userController.getFoodHistory = async (req, res, next) => {
   const { username } = req.body;
   const result = await User.findOne({ username }, (err, username) => {
     if (!username || err) {
-      console.log('user not found');
-      res.redirect('/api/signup');  
+      console.log("user not found");
+      res.redirect("/api/signup");
     }
   });
-  console.log('result', result);
+  console.log("result", result);
   res.locals.history = result.history;
   return next();
 };
 
 userController.updatePizzaHistory = async (req, res, next) => {
-  const pizza = 'Pizza';
+  const pizza = "Pizza";
   const { username } = res.locals;
-  await User.findOneAndUpdate({ username }, { $push: { history : pizza }}, (err, data) => {
-    if (err) {
-      console.log('err: ', err);
-      return next(err);
+  await User.findOneAndUpdate(
+    { username },
+    { $push: { history: pizza } },
+    (err, data) => {
+      if (err) {
+        console.log("err: ", err);
+        return next(err);
+      } else {
+        return next();
+      }
     }
-    else {
-      return next();
-    }
-  });
+  );
 };
 
-userController.passwordCompare = async (req,res,next) =>{
+userController.passwordCompare = async (req, res, next) => {
   //res.locals.password = req.query.password
   const { password, dbPassword } = res.locals;
   const passwordCompare = await bcrypt.compare(password, dbPassword);
@@ -70,12 +73,10 @@ userController.passwordCompare = async (req,res,next) =>{
   return next();
 };
 
-
-
 /**
  * to access the user's data after the user logs in and in order to acess the user's location,
  * prefer cousine and food/ answer the user got from the past
-*/
+ */
 userController.profileData = (req, res, next) => {
   // write code here
 };
--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = 3000;
+const cookieParser = require("cookie-parser");
 //const mongoose = require('mongoose');
 //const userController = require('./controllers/userController');
 //const cookieParser = require('cookie-parser');
 //const cuisineRouter = require('./routes/cuisine');
-const mapRouter = require('./routes/map');
-const mapController = require('./controllers/mapController');
-const restaurantRouter = require('./routes/restaurant');
-const userRouter = require('./routes/user');
+const mapRouter = require("./routes/map");
+const mapController = require("./controllers/mapController");
+const restaurantRouter = require("./routes/restaurant");
+const userRouter = require("./routes/user");
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 // activate the cookieParser
 //app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
 
-app.use('/map', mapRouter);
-app.use('/restaurant', restaurantRouter);
+app.use("/map", mapRouter);
+app.use("/restaurant", restaurantRouter);
 //app.use('/api', cuisineRouter);
-app.use('/login', userRouter);
+app.use("/login", userRouter);
 
 //if (process.env.NODE_ENV === 'production') {
-app.use('/build', express.static(path.join(__dirname, '../build')));
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+app.use("/build", express.static(path.join(__dirname, "../build")));
+app.get("/", (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, "../index.html"));
 });
 //}
 // error handlers
@@ -34,9 +36,9 @@ app.get('/', (req, res) => {
 // global error handler
 app.use(function (err, req, res, next) {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign(defaultErr, err);
   // console.log(errorObj.log);
